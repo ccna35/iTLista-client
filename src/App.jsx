0237375352f1d77e96c760531413b184b86a6097
ccna35 <shawkykhalil@gmail.com>
2023-05-22 22:44:12 +0300

    "Added loading screen"

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,25 @@ import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
 import NotFound from "./pages/NotFound";
+import { useEffect, useLayoutEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="bg-white w-screen h-screen grid place-items-center fixed top-0 left-0 transition-colors duration-500">
+        <p className="transition-all duration-500 w-16 h-16 bg-dark-blue-itlista rounded-full animate-ping" />
+      </div>
+    );
+  }
+
   return (
     <Routes>
       <Route path="/" element={<Root />}>
