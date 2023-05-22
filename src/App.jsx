3431ba830b30e3fe0bf92a844eb94edccdd1aba9
ccna35 <shawkykhalil@gmail.com>
2023-05-22 22:10:30 +0300

    "Replaced BrowserRouter with HashRouter"

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes, createBrowserRouter } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
         <Route path="/contact" element={<ContactUs />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
